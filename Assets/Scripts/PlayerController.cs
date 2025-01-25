e98b4dc6a1b7962cc89ee8b95e555abf64973f7d
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-01-25 12:53:14 +0100

    Worked on player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 200f;
+    [SerializeField] private float _moveSpeed = 200f;
+    [SerializeField] private float _jumpForce = 5f;
+
+    public float gravityScale = 1f;
+
     private float _horizontalInput = 0f;
     private Rigidbody2D _rBody;
+    private MicDetection _micDetector;
 
 
     void Start()
     {
         _rBody = GetComponent<Rigidbody2D>();
+        _micDetector = GetComponent<MicDetection>();
     }
 
 
     void FixedUpdate()
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
-        float horizontalMovement = _horizontalInput * moveSpeed * Time.deltaTime;
-        _rBody.linearVelocity = new Vector2(horizontalMovement, _rBody.linearVelocityY);
+        float horizontalMovement = _horizontalInput * _moveSpeed * Time.deltaTime;
+
+        // Ground check
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rBody.linearVelocityY = _jumpForce;
+        }
+
+        _rBody.linearVelocity = new Vector2(horizontalMovement, _rBody.linearVelocity.y);
     }
 }
